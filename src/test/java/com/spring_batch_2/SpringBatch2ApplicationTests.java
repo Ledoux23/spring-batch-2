34c62e0bd3f1d0c5d909e583bf632ca03bb05f3a
TESSOH TENE <michel.tessohtene@cgi.com>
2025-04-01 09:33:10 +0200

    first commit

---
+++ src/test/java/com/spring_batch_2/SpringBatch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_batch_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
